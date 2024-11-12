692c00955ffb16df1d4b65fab073e31591a773e0
Artem-Tev <artemt1011@gmail.com>
2024-11-12 18:29:19 +0300

    fixed

--- triangle.py
+++ triangle.py
@@ -1,9 +1,11 @@
 def area(a, h):
     """
     Принимает основание a и высоту h треугольника (int или float).
-    Возвращает половину произведения a и h (площадь треугольника) (int или float).
+    Возвращает половину произведения a и h
+    (площадь треугольника) (int или float).
     """
-    if not isinstance(a, (int, float)) or not isinstance(h, (int, float)):
+    if (not isinstance(a, (int, float)) or
+            not isinstance(h, (int, float))):
         raise TypeError("Неверный тип данных")
     return 0.5 * a * h
 
@@ -11,8 +13,11 @@ def area(a, h):
 def perimeter(a, b, c):
     """
     Принимает длины сторон a, b и c треугольника (int или float).
-    Возвращает сумму a, b и c (периметр треугольника) (int или float).
+    Возвращает сумму a, b и c
+    (периметр треугольника) (int или float).
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)) or not isinstance(c, (int, float)):
+    if (not isinstance(a, (int, float)) or
+            not isinstance(b, (int, float)) or
+            not isinstance(c, (int, float))):
         raise TypeError("Неверный тип данных")
     return a + b + c
