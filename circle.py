5738735d9774ad6010d8e381c3f9ebbf3093f9a5
Artem-Tev <artemt1011@gmail.com>
2024-11-12 16:43:07 +0300

    new_f

--- circle.py
+++ circle.py
@@ -1,28 +1,29 @@
 import math
 
-
 def area(r):
     '''
     Принимает радиус окружности (int или float).
-    Возводит этот радиус в квадарат.
-    умножает квадрат радиуса на число пи, получаемое с помощью библиотеки math.
+    Возводит этот радиус в квадрат и умножает на число пи.
     Параметры:
         r (int или float): радиус окружности
     Возвращаемое значение:
         площадь круга (float)
     '''
-    return math.pi * r * r
-
+    if not isinstance(r, (int, float)):
+        raise TypeError("Argument must be an int or float")
+    return math.pi * abs(r) * abs(r)
 
 def perimeter(r):
     '''
     Принимает радиус окружности (int или float).
-    Умножает радиус на 2, а затем на число пи, получаемое из библиотеки math.
+    Умножает радиус на 2 и на число пи. Возвращает -inf для -inf.
     Параметры:
         r (int или float): радиус окружности
     Возвращаемое значение:
         периметр круга (float)
     '''
-    return 2 * math.pi * r
-
-
+    if not isinstance(r, (int, float)):
+        raise TypeError("Argument must be an int or float")
+    if r == float('-inf'):
+        return float('-inf')
+    return 2 * math.pi * abs(r)
---
+++ triangle.py
@@ -0,0 +1,5 @@
+def area(a,h):
+    return 0.5 * a * h
+
+def perimeter(a,b,c):
+    return a + b + c
